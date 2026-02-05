f04a80f745ce24e19f273c1a59ba9fee3aa942c1
Bishal <bishaldash.22@gmail.com>
2026-02-05 18:38:23 +0530

    added edit option and fixed layout issues

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.get("/", function (req, res) {
 
     });
 });
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      return res.send("File not found");
+    }
+
+    res.render("show", {
+      filename: req.params.filename,
+      data: filedata
+    });
+  });
+});
 app.post("/create", function (req, res) {
   const filename = req.body.title.split(" ").join("");
 
@@ -30,6 +42,40 @@ app.post("/create", function (req, res) {
     res.redirect("/");
   });
 });
+
+app.get("/edit/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      return res.send("File not found");
+    }
+
+    res.render("edit", {
+      filename: req.params.filename,
+      data: filedata
+    });
+  });
+});
+app.post("/edit/:filename", function (req, res) {
+  const oldPath = `./files/${req.params.filename}`;
+  const newFilename = req.body.newtitle.split(" ").join("") + ".txt";
+  const newPath = `./files/${newFilename}`;
+
+  fs.writeFile(newPath, req.body.newdata, function (err) {
+    if (err) {
+      return res.send("Error updating file");
+    }
+
+    // remove old file if renamed
+    if (newPath !== oldPath) {
+      fs.unlink(oldPath, () => {});
+    }
+
+    res.redirect("/");
+  });
+});
+
+
+
 
 
 app.listen(3000, function () {
